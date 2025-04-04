0ea96041d64d1774d17d1baeea1b9778d3429ce7
Yuna-ux <143661914+Yuna-ux@users.noreply.github.com>
2025-04-04 19:16:32 -0300

    Update route.js

--- app/proxy.pac/route.js
+++ app/proxy.pac/route.js
@@ -1,15 +1,3 @@
-export function GET() {
-  const pacFile = `
-    function FindProxyForURL(url, host) {
-      if (shExpMatch(host, "*.local")) {
-        return "PROXY 127.0.0.1:8080";
-      }
-      return "DIRECT";
-    }
-  `;
-  return new Response(pacFile, {
-    headers: {
-      "Content-Type": "application/x-ns-proxy-autoconfig"
-    }
-  });
+function FindProxyForURL(url, host) {
+  return "PROXY yuna-ux.vercel.app:443";
 }
