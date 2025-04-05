200c6daeea77474fa99bda4db23522134c4081f9
Yuna-ux <143661914+Yuna-ux@users.noreply.github.com>
2025-04-05 18:55:05 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import '../styles/Home-page.css';
 
 const Layout = ({ children }) => {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Script Copier</title>
-      </head>
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <meta charSet="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Script Copier</title>
+            </head>
+            <body>{children}</body>
+        </html>
+    );
 };
 
 export default Layout;
