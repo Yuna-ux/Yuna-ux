290e775eccd626a06fedc33678d001672644ed03
Yuna-ux <143661914+Yuna-ux@users.noreply.github.com>
2025-04-04 22:35:03 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import luaparse from 'luaparse';
+const luaparse = require("./bin/luaparse")
 
 export default function Home() {
   const [inputCode, setInputCode] = useState('');
