c2b7c713fb7c83334a68121d8e6806084d68b48e
Yuna-ux <143661914+Yuna-ux@users.noreply.github.com>
2025-06-08 11:55:53 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,8 +4,7 @@ export default function Home() {
   return (
     <Layout>
       <div className="yuna-container">
-        <h1>Yuna On Top!</h1>
-        <p>Your content goes here</p>
+        <h1>print('Hello World')</h1>
       </div>
     </Layout>
   );
