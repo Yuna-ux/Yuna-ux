81b3b501dffc99791d2bdeff863e87492b3a0290
Yuna-ux <143661914+Yuna-ux@users.noreply.github.com>
2025-06-08 11:32:45 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,15 +1,13 @@
-'use client';
-
-import React from 'react';
-
-const SimpleNumberPage = () => {
+export default function Home() {
   return (
-    {
-        "data": 191816425,
-        "status": "success",
-        "timestamp": 1234567890
-    }
+    <main>
+      <h1>Serverside EndPoint</h1>
+      <pre>
+        {`
+local response = game:HttpGet("\104\116\116\112\115\58\47\47\121\117\110\97\45\117\120\46\118\101\114\99\101\108\46\97\112\112")
+local data = game:GetService("HttpService"):JSONDecode(response)
+print(data.data)`}
+      </pre>
+    </main>
   );
-};
-
-export default SimpleNumberPage;
+}
