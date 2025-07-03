7b3720d6eb75eebe594e8adae84e11f55b60a558
Yuna-ux <143661914+Yuna-ux@users.noreply.github.com>
2025-07-03 13:51:20 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
   webpack: (config) => {
     config.externals.push({
-      'utf-8-validate': 'commonjs utf-8-validate',
-      'bufferutil': 'commonjs bufferutil'
     })
     return config
   },
