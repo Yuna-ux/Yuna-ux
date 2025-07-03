511770b9cd68bd8fa4710611eab9a5777e4e6d62
Yuna-ux <143661914+Yuna-ux@users.noreply.github.com>
2025-07-03 14:21:57 -0300

    Update page.js

--- app/pt-BR/page.js
+++ app/pt-BR/page.js
@@ -1,1 +1,10 @@
-
+export default function HomePT() {
+  return (
+    <main style={{ padding: 20, fontFamily: 'Arial, sans-serif' }}>
+      <h1>Bem-vindo ao meu site de IP</h1>
+      <p>Este site retorna seu IP público na rota <code>/v1</code> em formato JSON.</p>
+      <p>Exemplo: <code>https://yuna-ux.vercel.app/v1</code></p>
+      <p>Exemplo2: <code>https://yuna-ux.vercel.app/v2</code></p>
+    </main>
+  )
+}
