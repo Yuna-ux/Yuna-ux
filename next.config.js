c84f1d241870846648dc2ca594dabd1786bb9039
Yuna-ux <143661914+Yuna-ux@users.noreply.github.com>
2025-07-03 14:05:40 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.externals.push({
-    })
-    return config
+  webpack(config) {
+    //config.externals.push({ })
+    return config;
   },
-  experimental: {
-    serverComponentsExternalPackages: ['@monaco-editor/react']
-  }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
