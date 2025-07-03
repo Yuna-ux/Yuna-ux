e1d77a3093d80f378c1fc42a50ff86899c9b520a
Yuna-ux <143661914+Yuna-ux@users.noreply.github.com>
2025-07-03 14:38:29 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,5 @@
-// app/en-US/route.js
-import { NextResponse } from 'next/server'
+import { redirect } from 'next/navigation'
 
-export async function GET(request) {
-  const { pathname } = request.nextUrl
-
-  if (pathname === '/') {
-    return NextResponse.redirect(new URL('/en-US', request.url))
-  }
-
-  return NextResponse.next()
+export default function Page() {
+  redirect('/en-US')
 }
