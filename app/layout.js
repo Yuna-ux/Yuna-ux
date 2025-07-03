2c5b52ab45f045c557ce6d1d117de7cfe1dd9d63
Yuna-ux <143661914+Yuna-ux@users.noreply.github.com>
2025-07-03 14:14:13 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,30 @@
 // import '../styles/home-style.css'
 // app/layout.js
 export const metadata = {
-  title: 'Ip info',
-  description: 'Returns the IP in a Json table',
+  title: 'IP Info',
+  description: 'Returns the IP in a JSON table',
+  keywords: ['IP', 'Next.js', 'API'],
+  authors: [{ name: 'Yuna-ux' }],
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+  },
+  openGraph: {
+    title: 'IP Info',
+    description: 'Returns the IP in a JSON table',
+    url: 'https://yuna-ux.vercel.app',
+    siteName: 'Yuna UX',
+    locale: 'en-US',
+    type: 'website',
+  },
+  alternates: {
+    canonical: 'https://yuna-ux.vercel.app',
+    languages: {
+      'pt-BR': 'https://yuna-ux.vercel.app/pt-BR',
+      'en-US': 'https://yuna-ux.vercel.app/en-US',
+    },
+  },
 }
 
 export default function RootLayout({ children }) {
