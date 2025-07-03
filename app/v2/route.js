a94cfed7675a544ce218d017d1bc5911fa8a8b99
Yuna-ux <143661914+Yuna-ux@users.noreply.github.com>
2025-07-03 12:20:33 -0300

    Create route.js

---
+++ app/v2/route.js
@@ -0,0 +1,15 @@
+// app/v2/route.js
+import { NextResponse } from 'next/server'
+
+export function GET(request) {
+  const forwardedFor = request.headers.get('x-forwarded-for')
+  const ip = forwardedFor?.split(',')[0].trim() || 'IP not found.'
+
+  console.log('Returning IP:', ip)
+
+  return NextResponse.json({
+    ip,
+    hora: new Date().toISOString(),
+    userAgent: request.headers.get('user-agent')
+  })
+}
