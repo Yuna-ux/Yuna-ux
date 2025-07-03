24b17d81d5eeec413fd8114cde553cccb0bc1742
Yuna-ux <143661914+Yuna-ux@users.noreply.github.com>
2025-07-03 14:26:13 -0300

    Update page.js

--- app/en-US/page.js
+++ app/en-US/page.js
@@ -1,1 +1,11 @@
+import { NextResponse } from 'next/server'
 
+export function Home(request) {
+  const { pathname } = request.nextUrl
+
+  if (pathname === '/en-US') {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+  
+  return NextResponse.next()
+}
