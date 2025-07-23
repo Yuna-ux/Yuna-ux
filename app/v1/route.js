c9376b9a9e4731fab6a7778ba04d0c1dfbd6181b
Rafael <143661914+Yuna-ux@users.noreply.github.com>
2025-07-23 19:50:24 -0300

    Update route.js

--- app/v1/route.js
+++ app/v1/route.js
@@ -5,6 +5,10 @@ export function GET(request) {
   const forwardedFor = request.headers.get('x-forwarded-for')
   const ip = forwardedFor?.split(',')[0].trim() || 'IP not found.'
 
+  if (ip == 'IP not found.') {
+    console.log(ip)
+    return
+  }
   console.log('Returning IP:', ip)
 
   return NextResponse.json({
