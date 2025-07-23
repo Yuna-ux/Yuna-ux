d3d398d50f69a74af798711a8aed8f7a4e0e796d
Rafael <143661914+Yuna-ux@users.noreply.github.com>
2025-07-23 20:44:14 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -43,7 +43,7 @@ export default function RootLayout({ children }) {
         {/* Favicon */}
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        //<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         
         {/* PWA */}
         <link rel="manifest" href="/manifest.webmanifest" />
