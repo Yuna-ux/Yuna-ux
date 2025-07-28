f467e3a2c6d8cc271ff175da2cb166730f8bd8b0
Rafael <143661914+Yuna-ux@users.noreply.github.com>
2025-07-28 15:14:59 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,10 +2,8 @@
 
 export default function Home() {
   return (
-    <div class="Main">
-      <h1>Meu Site Simples</h1>
+      <h1>Ip</h1>
       <p id="ip-display">Carregando IP...</p>
       <script src="client/ip-fetch.js" />
-    </div>
   );
 }
