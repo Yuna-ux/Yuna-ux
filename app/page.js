214a1ee54d0a890e978cc4b8f19d691b2d098004
Rafael <143661914+Yuna-ux@users.noreply.github.com>
2025-07-28 15:04:57 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,6 +3,6 @@
 export default function Home() {
     return (
         <h1>Welcome to my html website</h1>
-        <script src="/scripts/ip-fetch.js"/>
+        <script src="./ip-fetch.js"/>
     )
 }
