b6c7e15ab4d51c4bb27267bbef16e5f4bc604cff
Rafael <143661914+Yuna-ux@users.noreply.github.com>
2025-08-12 18:27:39 -0300

    Update superbase.js

--- app/libs/superbase.js
+++ app/libs/superbase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = "https://iswuhikrhyudrbpdcols.supabase.co";
-const SUPABASE_KEY = "sb_publishable_0AuLfovh-dGGHt-K5apnmw_Mic96wXo"; // Use a ANON KEY (não a secret!)
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_KEY
+);
