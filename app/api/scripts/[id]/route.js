82ef9f56eaa2f09a686b52fb20683b6cff4b6c8c
Rafael <143661914+Yuna-ux@users.noreply.github.com>
2025-08-12 19:00:34 -0300

    Update route.js

--- app/api/scripts/[id]/route.js
+++ app/api/scripts/[id]/route.js
@@ -1,14 +1,19 @@
-
 import supabase from '@/libs/supabase';
 
 export async function GET(req, { params }) {
-  const { data, error } = await supabase
+  const userAgent = req.headers.get('user-agent') || '';
+  
+  if (!userAgent.includes('Roblox')) {
+    return new Response('Acesso negado', { status: 403 });
+  }
+
+  const { data } = await supabase
     .from('scripts')
     .select('lua_code')
     .eq('id', params.id)
     .single();
 
-  if (error) return new Response('Script não existe!', { status: 404 });
+  if (!data) return new Response('Script não encontrado', { status: 404 });
 
   return new Response(data.lua_code, {
     headers: { 'Content-Type': 'text/plain' }
