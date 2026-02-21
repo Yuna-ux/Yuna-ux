0ad5d7559fb30801ff61e62c4ffb8a4928d95ed7
Rafael <143661914+Yuna-ux@users.noreply.github.com>
2026-02-21 19:57:51 -0300

    Change STUN server URL in RTCPeerConnection

--- app/pc/page.js
+++ app/pc/page.js
@@ -27,7 +27,7 @@ export default function PCPage() {
       videoRef.current.srcObject = stream;
 
       const pc = new RTCPeerConnection({
-        iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+        iceServers: [{ urls: "stun:stun1.voiceeclipse.net:3478" }],
         bundlePolicy: "max-bundle",
         rtcpMuxPolicy: "require"
       });
